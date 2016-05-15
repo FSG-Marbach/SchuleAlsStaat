045208bee3753a5a759398db755f6d2c35470213
AdmiralJo <admiraljo2013@gmail.com>
2016-05-15 17:45:24 +0200

    Added comonents, start with functions
    Added every component without ServerInformationPanel
    Next TODO: that it looks nice(correct the layout), complete the
    functions, remove colors,...; Colors = no function only help

--- src/accessControlClient/Gui.java
+++ src/accessControlClient/Gui.java
@@ -5,13 +5,21 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.JobAttributes;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.text.MaskFormatter;
 
 import essentials.Essentials;
 
@@ -21,7 +29,7 @@ import essentials.Essentials;
  *
  */
 
-public class Gui {
+public class Gui implements KeyListener, ActionListener {
 
 	private JFrame frame;
 	private JPanel panel, pnl_header, pnl_informationLeftSide, pnl_informationRightSide, pnl_input,
@@ -31,6 +39,8 @@ public class Gui {
 	private Color clr_background;
 	private Font fnt_header, fnt_title, fnt_normal;
 	private GridBagLayout gridbaglayout;
+	private JButton btn_login;
+	private JTextField txf_userid;
 	private int width, height;
 
 	// Later not used only for Tests
@@ -66,7 +76,6 @@ public class Gui {
 	public boolean fillGaps(String name, String classLevel, Integer attendance, Boolean state, String information) {
 
 		try {
-
 			lbl_name.setText(name);
 			lbl_class.setText(classLevel);
 			lbl_attendance.setText(attendance.toString() + " h");
@@ -82,6 +91,34 @@ public class Gui {
 			return false;
 		}
 
+		return true;
+	}
+
+	/**
+	 * The 'fillInformations' method allows the input data to appear on the user
+	 * interface
+	 * 
+	 * @param information
+	 *            Important messages to the student at the accesspoint
+	 * @return boolean if false, exception occurred
+	 *
+	 */
+
+	public boolean fillInformations(String information) {
+		try {
+			lbl_information.setText(information);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean setStudentId(String id) {
+		try {
+			txf_userid.setText(id);
+		} catch (Exception e) {
+			return false;
+		}
 		return true;
 	}
 
@@ -111,7 +148,7 @@ public class Gui {
 			frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 			frame.setLayout(null);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setUndecorated(true);
+			frame.setUndecorated(false);
 
 			lbl_header = new JLabel("Citizen Data Managment System - Zugangskontrollclient");
 			lbl_header.setFont(fnt_header);
@@ -147,6 +184,17 @@ public class Gui {
 			lbl_information = new JLabel("");
 			lbl_information.setFont(fnt_normal);
 
+			btn_login = new JButton("Aus-/Einloggen");
+			btn_login.setFont(fnt_normal);
+			btn_login.addActionListener(this);
+			btn_login.setBackground(Color.lightGray);
+			btn_login.setContentAreaFilled(false);
+			btn_login.setSelected(true);
+
+			txf_userid = new JTextField();
+			txf_userid.setFont(fnt_normal);
+			txf_userid.addKeyListener(this);
+
 			panel = new JPanel();
 			panel.setLayout(gridbaglayout);
 			panel.setBounds(0, 0, width, height);
@@ -160,6 +208,8 @@ public class Gui {
 			pnl_header = new JPanel();
 			pnl_header.setLayout(gridbaglayout);
 			pnl_header.setBackground(Color.MAGENTA);
+
+			Essentials.addComponent(pnl_header, gridbaglayout, lbl_header, 0, 0, 1, 1, 1, 1, new Insets(0, 0, 0, 0));
 
 			/**
 			 * The 'pnl_informationLeftSide' is on the leftside, gets 2/3 of the
@@ -190,6 +240,22 @@ public class Gui {
 			pnl_informationLeftSide.setLayout(gridbaglayout);
 			pnl_informationLeftSide.setBackground(Color.cyan);
 
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_nameTitle, 0, 0, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_name, 0, 1, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_classTitle, 0, 2, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_class, 0, 3, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_attendanceTitle, 0, 4, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_attendance, 0, 5, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_stateTitle, 0, 6, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_state, 0, 7, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
 			/**
 			 * The 'pnl_informationLeftSide' is on the leftside, gets 1/3 of the
 			 * width and is in the middle under the 'pnl_header'
@@ -202,25 +268,30 @@ public class Gui {
 			pnl_informationRightSide.setLayout(gridbaglayout);
 			pnl_informationRightSide.setBackground(Color.PINK);
 
-			// TODO Textfield for serialnumber
-			// TODO Button to accept serialnumber
-			// TODO Button to log the student in/out
+			Essentials.addComponent(pnl_informationRightSide, gridbaglayout, lbl_informationTitle, 0, 0, 1, 1, 1, 1,
+					new Insets(0, 0, 0, 0));
+			// Essentials.addComponent(pnl_informationRightSide, gridbaglayout,
+			// lbl_information, 0, 1, 1, 1, 1, 1,
+			// new Insets(0, 0, 0, 0));
+
 			pnl_input = new JPanel();
 			pnl_input.setLayout(gridbaglayout);
 			pnl_input.setBackground(Color.gray);
 
+			Essentials.addComponent(pnl_input, gridbaglayout, txf_userid, 0, 0, 1, 1, 1, 1, new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_input, gridbaglayout, btn_login, 1, 0, 1, 1, 1, 1, new Insets(0, 0, 0, 0));
+
 			// TODO Add Server Informations
 			pnl_serverinformation = new JPanel();
 			pnl_serverinformation.setLayout(new FlowLayout());
-			pnl_serverinformation.setBackground(clr_background);
-
-			Essentials.addComponent(panel, gridbaglayout, pnl_header, 0, 0, 2, 2, 0, 0, new Insets(0, 0, 0, 0));
-			Essentials.addComponent(panel, gridbaglayout, pnl_informationLeftSide, 0, 3, 2, 9, 0, 0,
+			pnl_serverinformation.setBackground(Color.orange);
+			Essentials.addComponent(panel, gridbaglayout, pnl_header, 1, 0, 3, 1, 1, 1, new Insets(0, 0, 0, 0));
+			Essentials.addComponent(panel, gridbaglayout, pnl_informationLeftSide, 1, 1, 2, 2, 1, 1,
 					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(panel, gridbaglayout, pnl_informationRightSide, 0, 11, 1, 9, 0, 0,
+			Essentials.addComponent(panel, gridbaglayout, pnl_informationRightSide, 3, 1, 1, 2, 1, 1,
 					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(panel, gridbaglayout, pnl_input, 0, 19, 3, 2, 0, 0, new Insets(0, 0, 0, 0));
-			Essentials.addComponent(panel, gridbaglayout, pnl_serverinformation, 0, 21, 3, 1, 0, 0,
+			Essentials.addComponent(panel, gridbaglayout, pnl_input, 1, 3, 3, 1, 1, 1, new Insets(0, 0, 0, 0));
+			Essentials.addComponent(panel, gridbaglayout, pnl_serverinformation, 1, 4, 3, 1, 1, 1,
 					new Insets(0, 0, 0, 0));
 
 			frame.add(panel);
@@ -232,6 +303,36 @@ public class Gui {
 	}
 
 	public Gui() {
+	}
+
+	public void keyPressed(KeyEvent EVT) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		char c = e.getKeyChar();
+		if (!(Character.isDigit(c) || (c == KeyEvent.VK_BACK_SPACE) || (c == KeyEvent.VK_DELETE))) {
+			e.consume();
+		}
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btn_login) {
+			if (txf_userid.getText().equals("")) {
+				JOptionPane.showMessageDialog(null, "The Student ID field is empty.", "Error while logging in!", JOptionPane.ERROR_MESSAGE);
+			} else {
+				System.out.println("Student ID: " + txf_userid.getText());
+			}
+		}
 
 	}
 
