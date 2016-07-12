5e726d7cc21fe9ccd44426e43b428cba83c12aa2
Grunzwanzling <maximilian@gaisberg-helfenberg.de>
2016-07-12 21:18:49 +0200

    Added a pretty useless snippet of code
    Because Felix thinks, writing a line of code is faster than writing an
    other line of code. I don't get why..

---
+++ src/server/DatabaseConnection.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author Maximilian
+ *
+ */
+public class DatabaseConnection {
+	private Connection connection;
+
+	/**
+	 * 
+	 * @param connection
+	 *            A String that represents the connection. Has to be formated as
+	 *            following:
+	 *            jdbc:mysql://localhost/feedback?user=sqluser&password
+	 *            =sqluserpw
+	 * @throws ClassNotFoundException
+	 *             Will be thrown if the driver couldn't be loaded
+	 * @throws SQLException
+	 *             Will be thrown if the connection was not possible
+	 */
+	public DatabaseConnection(String connection) throws ClassNotFoundException,
+			SQLException {
+
+		Class.forName("com.mysql.jdbc.Drive");
+		this.connection = DriverManager.getConnection(connection);
+	}
+
+	/**
+	 * 
+	 * @param query
+	 *            The query to be executed
+	 * @return The ResultSet
+	 * @throws SQLException
+	 *             If something went wrong
+	 */
+	public ResultSet executeQuery(String query) throws SQLException {
+
+		return connection.createStatement().executeQuery(query);
+
+	}
+
+}
