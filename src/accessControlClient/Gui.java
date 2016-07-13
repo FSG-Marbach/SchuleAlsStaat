c2d1c991114b515682784b0d80543bdfce8fe9ac
AdmiralJo <admiraljo2013@gmail.com>
2016-07-13 11:26:12 +0200

    restored accescontrollclient after Github crash

--- src/accessControlClient/Gui.java
+++ src/accessControlClient/Gui.java
@@ -1,25 +1,30 @@
 package accessControlClient;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
-import java.awt.JobAttributes;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
-import javax.swing.text.MaskFormatter;
 
 import essentials.Essentials;
 
@@ -33,25 +38,24 @@ public class Gui implements KeyListener, ActionListener {
 
 	private JFrame frame;
 	private JPanel panel, pnl_header, pnl_informationLeftSide, pnl_informationRightSide, pnl_input,
-			pnl_serverinformation;
+			pnl_serverinformation, pnl_imagePlace;
 	private JLabel lbl_header, lbl_name, lbl_nameTitle, lbl_class, lbl_classTitle, lbl_attendance, lbl_attendanceTitle,
-			lbl_state, lbl_stateTitle, lbl_informationTitle, lbl_information;
-	private Color clr_background;
+			lbl_informationTitle, lbl_image, lbl_imageTitle;
+	private Color clr_background, clr_backgroundHeader;
 	private Font fnt_header, fnt_title, fnt_normal;
 	private GridBagLayout gridbaglayout;
 	private JButton btn_login;
-	private JTextField txf_userid;
+	private JTextArea jta_information;
+	private JScrollPane scp_information;
+	private static JTextField txf_userid;
 	private int width, height;
-
-	// Later not used only for Tests
-	private JPanel rest;
 
 	public void restoreDefaults() {
 		lbl_name.setText("");
 		lbl_class.setText("");
 		lbl_attendance.setText("");
-		lbl_state.setText("");
-		lbl_information.setText("");
+		jta_information.setText("");
+		txf_userid.setText("");
 	}
 
 	/**
@@ -79,14 +83,7 @@ public class Gui implements KeyListener, ActionListener {
 			lbl_name.setText(name);
 			lbl_class.setText(classLevel);
 			lbl_attendance.setText(attendance.toString() + " h");
-			if (state) {
-				lbl_state.setForeground(Color.GREEN);
-				lbl_state.setText("Ja");
-			} else {
-				lbl_state.setForeground(Color.RED);
-				lbl_state.setText("Nein");
-			}
-			lbl_information.setText(information);
+			jta_information.setText(information);
 		} catch (Exception e) {
 			return false;
 		}
@@ -106,15 +103,18 @@ public class Gui implements KeyListener, ActionListener {
 
 	public boolean fillInformations(String information) {
 		try {
-			lbl_information.setText(information);
+			jta_information.setText(information);
 		} catch (Exception e) {
 			return false;
 		}
 		return true;
 	}
 
-	public boolean setStudentId(String id) {
+	public static boolean setStudentId(String id) {
 		try {
+
+			id = id.replace("<", "");
+			id = id.replace(">", "");
 			txf_userid.setText(id);
 		} catch (Exception e) {
 			return false;
@@ -130,6 +130,7 @@ public class Gui implements KeyListener, ActionListener {
 	 **/
 
 	public boolean initialize() {
+
 		try {
 
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -137,165 +138,169 @@ public class Gui implements KeyListener, ActionListener {
 			width = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 			height = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 
-			clr_background = new Color(10, 240, 240);
-			fnt_header = new Font("Arial Rounded MT Bold", Font.BOLD, 20);
-			fnt_title = new Font("Arial Rounded MT Bold", Font.BOLD, 14);
-			fnt_title = new Font("Arial Rounded MT Bold", 0, 14);
+			width = 1366;
+			height = 760;
+
+			fnt_title = new Font("Arial Rounded MT Bold", Font.BOLD, 22);
+			fnt_normal = new Font("Arial Rounded MT Bold", 0, 18);
+
+			clr_background = new Color(240, 240, 240);
+			clr_backgroundHeader = new Color(180, 180, 180);
+
 			gridbaglayout = new GridBagLayout();
 
 			frame = new JFrame();
 			frame.setSize(width, height);
-			frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 			frame.setLayout(null);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setUndecorated(false);
-
-			lbl_header = new JLabel("Citizen Data Managment System - Zugangskontrollclient");
-			lbl_header.setFont(fnt_header);
-			lbl_header.setForeground(Color.black);
+			frame.setUndecorated(true);
 
 			lbl_name = new JLabel("");
 			lbl_name.setFont(fnt_normal);
+			lbl_name.setHorizontalAlignment(SwingConstants.LEFT);
 
 			lbl_nameTitle = new JLabel("Name:");
 			lbl_nameTitle.setFont(fnt_title);
+			lbl_nameTitle.setHorizontalAlignment(SwingConstants.LEFT);
 
 			lbl_class = new JLabel("");
 			lbl_class.setFont(fnt_normal);
+			lbl_class.setHorizontalAlignment(SwingConstants.LEFT);
 
 			lbl_classTitle = new JLabel("Klasse:");
 			lbl_classTitle.setFont(fnt_title);
+			lbl_classTitle.setHorizontalAlignment(SwingConstants.LEFT);
 
 			lbl_attendance = new JLabel("");
 			lbl_attendance.setFont(fnt_normal);
+			lbl_attendance.setHorizontalAlignment(SwingConstants.LEFT);
 
 			lbl_attendanceTitle = new JLabel("Bissherige Anwesenheitszeit:");
 			lbl_attendanceTitle.setFont(fnt_title);
-
-			lbl_state = new JLabel("");
-			lbl_state.setFont(fnt_normal);
-
-			lbl_stateTitle = new JLabel("Person ist anwesend:");
-			lbl_stateTitle.setFont(fnt_title);
+			lbl_attendanceTitle.setHorizontalAlignment(SwingConstants.LEFT);
 
 			lbl_informationTitle = new JLabel("Wichtige Informationen:");
 			lbl_informationTitle.setFont(fnt_title);
+			lbl_informationTitle.setHorizontalAlignment(SwingConstants.LEFT);
 
-			lbl_information = new JLabel("");
-			lbl_information.setFont(fnt_normal);
+			jta_information = new JTextArea("Keine Informationen verfügbar...");
+			jta_information.setFont(fnt_normal);
+			jta_information.setLineWrap(true);
+			jta_information.setWrapStyleWord(true);
+			jta_information.setEditable(false);
+			jta_information.setBackground(clr_background);
+			jta_information.setSelectedTextColor(Color.black);
+			jta_information.setSelectionColor(clr_background);
+
+			scp_information = new JScrollPane(jta_information);
+			scp_information.setBorder(BorderFactory.createEmptyBorder());
+
+			lbl_imageTitle = new JLabel("Foto:");
+			lbl_imageTitle.setFont(fnt_title);
+			lbl_imageTitle.setHorizontalAlignment(SwingConstants.LEFT);
+
+			lbl_image = new JLabel();
+			lbl_image.setHorizontalAlignment(SwingConstants.CENTER);
 
 			btn_login = new JButton("Aus-/Einloggen");
 			btn_login.setFont(fnt_normal);
 			btn_login.addActionListener(this);
-			btn_login.setBackground(Color.lightGray);
-			btn_login.setContentAreaFilled(false);
-			btn_login.setSelected(true);
+			btn_login.setContentAreaFilled(true);
 
 			txf_userid = new JTextField();
 			txf_userid.setFont(fnt_normal);
 			txf_userid.addKeyListener(this);
+			txf_userid.requestFocus(true);
 
 			panel = new JPanel();
-			panel.setLayout(gridbaglayout);
+			panel.setLayout(null);
 			panel.setBounds(0, 0, width, height);
 			panel.setBackground(clr_background);
 
-			/**
-			 * The 'pnl_header' is at the top
-			 * 
-			 * @add 'lbl_header'
-			 */
 			pnl_header = new JPanel();
-			pnl_header.setLayout(gridbaglayout);
-			pnl_header.setBackground(Color.MAGENTA);
+			pnl_header.setLayout(new BorderLayout());
+			pnl_header.setBackground(clr_backgroundHeader);
 
-			Essentials.addComponent(pnl_header, gridbaglayout, lbl_header, 0, 0, 1, 1, 1, 1, new Insets(0, 0, 0, 0));
-
-			/**
-			 * The 'pnl_informationLeftSide' is on the leftside, gets 2/3 of the
-			 * width and is in the middle under the 'pnl_header'
-			 * 
-			 * @add 'lbl_nameTitle' The title over the student´s name
-			 * 
-			 * @add 'lbl_name' shows the name of the student
-			 * 
-			 * @add 'lbl_classTitle' The title over the student´s classlevel
-			 * 
-			 * @add 'lbl_class' shows the classlevel of the student
-			 *
-			 * @add 'lbl_attendanceTitle' The title over the student´s
-			 *      attendance
-			 * 
-			 * @add 'lbl_attendance' shows the attendance time of the student
-			 * 
-			 * @add 'lbl_stateTitle' The title over the student´s state
-			 * 
-			 * @add 'lbl_state' shows the state of the student (in-/outside the
-			 *      state)
-			 * 
-			 */
-
-			// TODO Add Image
 			pnl_informationLeftSide = new JPanel();
 			pnl_informationLeftSide.setLayout(gridbaglayout);
-			pnl_informationLeftSide.setBackground(Color.cyan);
+			pnl_informationLeftSide.setBackground(clr_background);
 
-			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_nameTitle, 0, 0, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_name, 0, 1, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_classTitle, 0, 2, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_class, 0, 3, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_attendanceTitle, 0, 4, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_attendance, 0, 5, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_stateTitle, 0, 6, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_state, 0, 7, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			/**
-			 * The 'pnl_informationLeftSide' is on the leftside, gets 1/3 of the
-			 * width and is in the middle under the 'pnl_header'
-			 * 
-			 * @add 'lbl_informationTitle' The title over the messages
-			 * 
-			 * @add 'lbl_information' shows important messages
-			 */
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_nameTitle, 0, 0, 2, 1, 1, 1,
+					new Insets(5, 5, 5, 5), GridBagConstraints.WEST);
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_name, 0, 1, 2, 1, 1, 1,
+					new Insets(5, 5, 5, 5), GridBagConstraints.WEST);
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_classTitle, 0, 2, 2, 1, 1, 1,
+					new Insets(5, 5, 5, 5), GridBagConstraints.WEST);
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_class, 0, 3, 2, 1, 1, 1,
+					new Insets(5, 5, 5, 5), GridBagConstraints.WEST);
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_attendanceTitle, 0, 4, 2, 1, 1, 1,
+					new Insets(5, 5, 5, 5), GridBagConstraints.WEST);
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, lbl_attendance, 0, 5, 2, 1, 1, 1,
+					new Insets(5, 5, 5, 5), GridBagConstraints.WEST);
+
+			pnl_imagePlace = new JPanel();
+			pnl_imagePlace.setLayout(new BorderLayout());
+			pnl_imagePlace.setBackground(clr_background);
+
+			ImageIcon image = new ImageIcon("res/client/emptyprofilpic.png");
+			image.setImage(image.getImage().getScaledInstance(250, 350, Image.SCALE_DEFAULT));
+			lbl_image.setIcon(image);
+
+			pnl_imagePlace.add(lbl_imageTitle, BorderLayout.NORTH);
+			pnl_imagePlace.add(lbl_image, BorderLayout.CENTER);
+
+			Essentials.addComponent(pnl_informationLeftSide, gridbaglayout, pnl_imagePlace, 2, 0, 1, 5, 0, 1,
+					new Insets(5, 5, 5, 5));
+
 			pnl_informationRightSide = new JPanel();
-			pnl_informationRightSide.setLayout(gridbaglayout);
-			pnl_informationRightSide.setBackground(Color.PINK);
+			pnl_informationRightSide.setLayout(new BorderLayout());
+			pnl_informationRightSide.setBackground(clr_background);
 
-			Essentials.addComponent(pnl_informationRightSide, gridbaglayout, lbl_informationTitle, 0, 0, 1, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
-			// Essentials.addComponent(pnl_informationRightSide, gridbaglayout,
-			// lbl_information, 0, 1, 1, 1, 1, 1,
-			// new Insets(0, 0, 0, 0));
+			pnl_informationRightSide.add(lbl_informationTitle, BorderLayout.NORTH);
+			pnl_informationRightSide.add(scp_information, BorderLayout.CENTER);
 
 			pnl_input = new JPanel();
 			pnl_input.setLayout(gridbaglayout);
-			pnl_input.setBackground(Color.gray);
+			pnl_input.setBackground(clr_background);
 
-			Essentials.addComponent(pnl_input, gridbaglayout, txf_userid, 0, 0, 1, 1, 1, 1, new Insets(0, 0, 0, 0));
-			Essentials.addComponent(pnl_input, gridbaglayout, btn_login, 1, 0, 1, 1, 1, 1, new Insets(0, 0, 0, 0));
+			Essentials.addComponent(pnl_input, gridbaglayout, txf_userid, 0, 0, 1, 1, 1, 1, new Insets(15, 15, 15, 15));
+			Essentials.addComponent(pnl_input, gridbaglayout, btn_login, 1, 0, 1, 1, 1, 1, new Insets(15, 15, 15, 15));
 
-			// TODO Add Server Informations
 			pnl_serverinformation = new JPanel();
-			pnl_serverinformation.setLayout(new FlowLayout());
-			pnl_serverinformation.setBackground(Color.orange);
-			Essentials.addComponent(panel, gridbaglayout, pnl_header, 1, 0, 3, 1, 1, 1, new Insets(0, 0, 0, 0));
-			Essentials.addComponent(panel, gridbaglayout, pnl_informationLeftSide, 1, 1, 2, 2, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(panel, gridbaglayout, pnl_informationRightSide, 3, 1, 1, 2, 1, 1,
-					new Insets(0, 0, 0, 0));
-			Essentials.addComponent(panel, gridbaglayout, pnl_input, 1, 3, 3, 1, 1, 1, new Insets(0, 0, 0, 0));
-			Essentials.addComponent(panel, gridbaglayout, pnl_serverinformation, 1, 4, 3, 1, 1, 1,
-					new Insets(0, 0, 0, 0));
+			pnl_serverinformation.setLayout(gridbaglayout);
+			pnl_serverinformation.setBackground(Color.green);
+
+			pnl_header.setBounds(0, 0, width, height / 7);
+			pnl_informationLeftSide.setBounds(0, height / 7, width / 3 * 2, height / 7 * 4);
+			pnl_informationRightSide.setBounds(width / 3 * 2, height / 7, width / 3, height / 7 * 4);
+			pnl_input.setBounds(0, height / 7 * 5, width, height / 7);
+			pnl_serverinformation.setBounds(0, height / 7 * 6, width, height / 7);
+
+			fnt_header = new Font("Arial Rounded MT Bold", Font.BOLD, (int) pnl_header.getSize().getHeight() / 3);
+
+			lbl_header = new JLabel("Citizen Data Managment System - Zugangskontrollclient");
+			lbl_header.setFont(fnt_header);
+			lbl_header.setForeground(Color.black);
+			lbl_header.setHorizontalAlignment(SwingConstants.CENTER);
+			lbl_header.setVerticalAlignment(SwingConstants.CENTER);
+
+			pnl_header.add(lbl_header, BorderLayout.CENTER);
+
+			pnl_header.setBorder(BorderFactory.createLineBorder(clr_background));
+			pnl_informationLeftSide.setBorder(BorderFactory.createLineBorder(clr_background));
+			pnl_informationRightSide.setBorder(BorderFactory.createLineBorder(clr_background));
+			pnl_input.setBorder(BorderFactory.createLineBorder(clr_background));
+			pnl_serverinformation.setBorder(BorderFactory.createLineBorder(clr_background));
+
+			panel.add(pnl_header);
+			panel.add(pnl_informationLeftSide);
+			panel.add(pnl_informationRightSide);
+			panel.add(pnl_input);
+			panel.add(pnl_serverinformation);
 
 			frame.add(panel);
 			frame.setVisible(true);
+			txf_userid.requestFocus(true);
 		} catch (Exception e) {
 			return false;
 		}
@@ -303,6 +308,7 @@ public class Gui implements KeyListener, ActionListener {
 	}
 
 	public Gui() {
+
 	}
 
 	public void keyPressed(KeyEvent EVT) {
@@ -328,10 +334,15 @@ public class Gui implements KeyListener, ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == btn_login) {
 			if (txf_userid.getText().equals("")) {
-				JOptionPane.showMessageDialog(null, "The Student ID field is empty.", "Error while logging in!", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "The Student ID field is empty.", "Error while logging in!",
+						JOptionPane.ERROR_MESSAGE);
 			} else {
 				System.out.println("Student ID: " + txf_userid.getText());
+				restoreDefaults();
 			}
+
+			txf_userid.requestFocus();
+
 		}
 
 	}
--- src/accessControlClient/Main.java
+++ src/accessControlClient/Main.java
@@ -1,5 +1,10 @@
 package accessControlClient;
 
+import javax.swing.JOptionPane;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+
 /**
  * @author Johannes Groﬂ
  */
@@ -8,6 +13,20 @@ public class Main {
 	public static void main(String[] args) {
 		Gui gui = new Gui();
 		gui.initialize();
+
+		try {
+			GlobalScreen.registerNativeHook();
+		} catch (NativeHookException ex) {
+			JOptionPane.showMessageDialog(null,
+					"Es ist ein Problem beim Erstellen des Barcodelisteners aufgetreten, bitte informieren Sie einen zust‰ndigen Administrator.",
+					"Barcodelistenerfehler", JOptionPane.ERROR_MESSAGE);
+
+			System.err.println("There was a problem registering the barcodescannerlisterner.");
+			System.err.println(ex.getMessage());
+		}
+
+		GlobalScreen.getInstance().addNativeKeyListener(new Scannerlistener());
+
 	}
 
 }
