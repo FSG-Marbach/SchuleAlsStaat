36687ce99e15c137bcac1f8a242bd50d07ba03a7
AdmiralJo <admiraljo2013@gmail.com>
2016-07-18 01:11:09 +0200

    changed timeformat at attendancecalculation

--- src/accessControl/Gui.java
+++ src/accessControl/Gui.java
@@ -420,8 +420,8 @@ public class Gui implements KeyListener, ActionListener {
 				Main.connection.writeLine("getTodaysDate");
 				long currentmillis = Long.parseLong(Main.connection.readLine());
 
-				Timestamp time = new Timestamp(currentmillis);
-				Date today = new Date(time.getTime());
+				Timestamp todaytimestamp = new Timestamp(currentmillis);
+				Date today = new Date(todaytimestamp.getTime());
 
 				String attendance = calculateAttendanceTime(studentid, loginarray, logoutarray, dateFormat, today);
 
@@ -443,8 +443,10 @@ public class Gui implements KeyListener, ActionListener {
 		try {
 			for (int i = 0; i < logoutarray.length; i++) {
 
-				Date from = dateFormat.parse(loginarray[i]);
-				Date to = dateFormat.parse(logoutarray[i]);
+				Timestamp logintimestamp = new Timestamp(Long.parseLong(loginarray[i]));
+				Timestamp logouttimestamp = new Timestamp(Long.parseLong(logoutarray[i]));
+				Date from = new Date(logintimestamp.getTime());
+				Date to = new Date(logouttimestamp.getTime());
 
 				if (today.getDate() == from.getDate()) {
 					diffMillis = diffMillis + to.getTime() - from.getTime();
@@ -456,7 +458,7 @@ public class Gui implements KeyListener, ActionListener {
 		}
 
 		diffMillis = diffMillis / 1000;
-
+		
 		final int MINUTES_IN_AN_HOUR = 60;
 		final int SECONDS_IN_A_MINUTE = 60;
 
@@ -464,6 +466,8 @@ public class Gui implements KeyListener, ActionListener {
 		int totalMinutes = (int) (diffMillis / SECONDS_IN_A_MINUTE);
 		int minutes = totalMinutes % MINUTES_IN_AN_HOUR;
 		int hours = totalMinutes / MINUTES_IN_AN_HOUR;
+
+//		System.out.println(hours + "h " + minutes + "min " + seconds + "s");
 
 		return hours + "h " + minutes + "min " + seconds + "s";
 
